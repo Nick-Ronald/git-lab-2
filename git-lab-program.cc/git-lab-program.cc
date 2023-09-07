9350063df6973a4bbacdce1a3a5f7b6db1a42a90
Nick-Ronald <nr737922@ohio.edu>
2023-09-07 17:10:50 -0400

    Github Lab Program

---
+++ git-lab-program.cc/git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+    File: git-lab-program.cc
+    Author: Nick Ronald
+    Date: 9/7/2023
+    Description: Lab 2
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Hello Git!!" << endl;
+}
